95da1062ce8020b76a737e77ffaf51fb7c1b9fd0
iLogical <iLogical8502@gmail.com>
2021-01-20 16:37:23 +0000

    Moving the SceneAsset into its own file

---
+++ GameCore.Shared/Scene/SceneAsset.cs
@@ -0,0 +1,25 @@
+﻿using Ultraviolet;
+
+namespace GameCore.GameObjects
+{
+    public class SceneAsset
+    {
+        public AssetType Type { get; }
+        public string Name { get; }
+        public string Resource { get; }
+        public Vector2 Position { get; }
+
+        public static SceneAsset For(string name, AssetType type, string resource, Vector2 position)
+        {
+            return new(name, type, resource, position);
+        }
+
+        private SceneAsset(string name, AssetType type, string resource, Vector2 position)
+        {
+            Type = type;
+            Name = name;
+            Resource = resource;
+            Position = position;
+        }
+    }
+}
