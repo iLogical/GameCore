d56aeb5ac0b8a36d022c341b97b9dda44c3bdb62
iLogical <ilogical8502@gmail.com>
2021-01-20 22:11:45 +0000

    added more protobuf manipulations

--- GameCore.Shared/Storage/ProtobufStore.cs
+++ GameCore.Shared/Storage/ProtobufStore.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using ProtoBuf;
 
 namespace GameCore.Storage
@@ -15,6 +16,18 @@ namespace GameCore.Storage
         {
             using var file = File.OpenRead(filePath);
             return Serializer.Deserialize<T>(file);
+        }
+
+        public static string Serialize<T>(T protobufObject)
+        {
+            var memoryStream = new MemoryStream();
+            Serializer.Serialize(memoryStream, protobufObject);
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        public static T Deserialize<T>(string encodedObject)
+        {
+            return Serializer.Deserialize<T>(new MemoryStream(Convert.FromBase64String(encodedObject)));
         }
     }
 }
